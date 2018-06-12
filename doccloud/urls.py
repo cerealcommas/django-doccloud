cceceb5d9c18de72cc867f8ef1e6ebebed6c9d13
Sergio Hernandez <sergio.hernandez@cnn.com>
2018-06-12 12:00:20 -0400

    update urls

--- doccloud/urls.py
+++ doccloud/urls.py
@@ -1,20 +1,13 @@
-from django.urls import include, path
+# from django.conf.urls import url, patterns
 # from django.conf.urls.defaults import patterns
-# from django.conf.urls.defaults import include
+from django.conf.urls.defaults import include, re_path
 # from django.conf.urls.defaults import url
 
-urlpatterns = [
-    path('', 'doccloud.views.list', name='docs_list'),
-    path('list/', 'doccloud.views.list', name='docs_list'),
-    path('detail/<slug:slug>', 'doccloud.views.detail', name='docs_detail'),
-    path('create/', 'doccloud.views.create', name='docs_create'),
-    path('upload/', 'doccloud.views.upload', name='docs_upload'),
-]
-# patterns('',
-#     url(r'^$', 'doccloud.views.list', name='docs_list'),
-#     url(r'^list/$', 'doccloud.views.list', name='docs_list'),
-#     url(r'^detail/(?P<slug>[\w-]+)/$', 'doccloud.views.detail',
-#         name='docs_detail'),
-#     url(r'^create/$', 'doccloud.views.create', name='docs_create'),
-#     url(r'^upload/$', 'doccloud.views.upload', name='docs_upload'),
-# )
+urlpatterns = patterns('',
+    re_path(r'^$', 'doccloud.views.list', name='docs_list'),
+    re_path(r'^list/$', 'doccloud.views.list', name='docs_list'),
+    re_path(r'^detail/(?P<slug>[\w-]+)/$', 'doccloud.views.detail',
+        name='docs_detail'),
+    re_path(r'^create/$', 'doccloud.views.create', name='docs_create'),
+    re_path(r'^upload/$', 'doccloud.views.upload', name='docs_upload'),
+)
