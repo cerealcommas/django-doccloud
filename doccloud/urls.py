de2b5e14aa874b27432eab7bcb0b8666c85685ae
Sergio Hernandez <sergio.hernandez@cnn.com>
2018-06-12 12:03:33 -0400

    change conf.urls to urls

--- doccloud/urls.py
+++ doccloud/urls.py
@@ -3,11 +3,11 @@
 from django.urls import include, re_path
 # from django.conf.urls.defaults import url
 
-urlpatterns = patterns('',
+urlpatterns = [
     re_path(r'^$', 'doccloud.views.list', name='docs_list'),
     re_path(r'^list/$', 'doccloud.views.list', name='docs_list'),
     re_path(r'^detail/(?P<slug>[\w-]+)/$', 'doccloud.views.detail',
         name='docs_detail'),
     re_path(r'^create/$', 'doccloud.views.create', name='docs_create'),
     re_path(r'^upload/$', 'doccloud.views.upload', name='docs_upload'),
-)
+]
